d45119827a4ebfe3c7e3843ab94a579ac7c49e04
vi <viktnnchik@mail.ru>
2022-10-18 19:21:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-console.log('Hello world!');
+console.log("Hello world!");
 let OurPlanetName = "Земля";
 console.log(OurPlanetName);
+const name = "Viktor";
+const age = 18;
+const userData = `Добро пожаловать ${name},Ваш возраст ${age}?`;
+console.log(userData);
+const isUserVisible = true;
