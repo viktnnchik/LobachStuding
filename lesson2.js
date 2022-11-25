d6ae5a543754fbbefffd91fda5661c3d24de3774
vi <viktnnchik@mail.ru>
2022-11-25 19:50:28 +0300

    z

--- lesson2.js
+++ lesson2.js
@@ -1,6 +1,12 @@
 const name = "Viktor";
-
-
 const nameToNumber = Number(name);
-
-console.log(nameToNumber)
+console.log(name)
+const age=18;
+const ageToString = String(age);
+console.log(typeof ageToString);
+console.log(typeof age);
+const nullValue = null;
+console.log(Number(nullValue));
+const o = Boolean(nullValue)
+console.log(o)
+console.log(typeof o)
