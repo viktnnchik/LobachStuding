d8492d6e0588b2cee5a38523b65b86b1a38286d2
vi <viktnnchik@mail.ru>
2022-11-25 22:00:21 +0300

    cycles.js update

---
+++ cycles.js
@@ -0,0 +1,11 @@
+console.log("true");
+console.log("true");
+let name = "Viktor";
+while (true){
+    if(name === "Viktor"){
+        console.log(`Привет, ${name}`);
+        name = "Gena";
+    }else{
+        console.log(`Привет, ${name}`);
+    }
+}
