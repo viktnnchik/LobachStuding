87b3bbdf5ab210bb75951f8d9a3b1fc645add614
vi <viktnnchik@mail.ru>
2022-11-25 23:10:39 +0300

    cycles.js update

--- cycles.js
+++ cycles.js
@@ -26,3 +26,48 @@ if (name1 === "gg1") {
 } else {
     console.log(3)
 }
+let z = name === "Viktor" ? 2 : 3;
+console.log(z)
+const age="18";
+switch (age){
+    case "18":
+        console.log(18);
+        break;
+    case 17:
+        console.log(17);
+        break;
+    default:
+        console.log(16);
+        break;
+}
+
+const lenght1=39;
+const lenght2=45;
+const lenght3=79;
+if (lenght1>lenght2){
+    if (lenght1>lenght3){
+        console.log(lenght1);
+    }else {
+        console.log(lenght3)
+    }
+}else if (lenght2>lenght3){
+    console.log(lenght2)
+}else {
+    console.log(lenght3)
+}
+
+const man1 = "admin"
+const man2 = "mod"
+const man3 = "user"
+
+switch (man3){
+    case "admin":
+        console.log(man1);
+        break;
+    case  "mod":
+        console.log(man2);
+        break;
+    case "user":
+        console.log(man3);
+        break;
+}
