3df7e96a051e848485d3602fdfefe75cc5c37afb
vi <viktnnchik@mail.ru>
2022-11-30 17:58:43 +0300

    function update

--- NewFunction.js
+++ NewFunction.js
@@ -3,7 +3,27 @@
 //}
 //
 //showMessage("Hi")
-function sum(a,b=0){
-    console.log(a+b)
-}
-sum(2)
+//unction sum(a,b=0){
+//   console.log(a+b)
+//
+//sum(2)
+//let name = 'Viktor'
+//
+//function sayHi(text= "hi"){
+//    console.log(`${text} ${name}`)
+//}
+//sayHi()
+//sayHi('Привет')
+//function UserName(Name="User"){
+//    return (
+//        `hi ${Name}`
+//    )
+//}
+//console.log(UserName('Viktor'))
+//function Degree(MainNumber,degree=1){
+//   return(
+//       MainNumber**degree
+//   )
+//}
+//console.log(Degree(5,25))
+
