cf521ede81c56da5959a38eb27f2bd2cc21444a0
vi <viktnnchik@mail.ru>
2022-12-02 18:29:55 +0300

    function update

---
+++ function.js
@@ -0,0 +1,37 @@
+let MyfavouriteFilm = {
+    name: "The lord of rings",
+    date: 2010,
+    sayName: function () {
+        console.log("The lord of rings");
+    },
+};
+MyfavouriteFilm.money = 1000;
+delete MyfavouriteFilm.date;
+console.log(MyfavouriteFilm);
+MyfavouriteFilm.sayName();
+let user = {
+    sayName: function () {
+        console.log("Ivan");
+    },
+};
+user.sayName();
+
+let str = "Hello Viktor";
+console.log(str);
+
+let number = 1.5534343;
+console.log(number.toFixed(2));
+let num = 255;
+console.log(num.toString());
+console.log(num.toString(8));
+console.log(num.toString(2));
+let func = (number, word) => {
+    let big = word[number];
+    console.log(big.toUpperCase());
+    if (word[number] > big.toUpperCase()) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+};
+func(6, "BadxxxxX");
